66b86322c13df78dc99853c18d89fac4f9b4e5bc
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-11-16 21:52:28 +0100

    up
    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistic/Statistic';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Container } from './App.styled';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onBtnClick = (actionType) => {
+    switch (actionType) {
+      case 'good':
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        console.error('Invalid action type');
+    }
   };
 
-  onBtnClick = actionType => {
-    this.setState(prevState => ({
-      [actionType]: prevState[actionType] + 1,
-    }));
-  };
+ 
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedback = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countTotalFeedback = () => good + neutral + bad;
+ const total = countTotalFeedback();
+  const countPositiveFeedback = () => {
+    const total = countTotalFeedback();
     return total > 0 ? ((good / total) * 100).toFixed(0) : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedback();
+  return (
+    <Container>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions
+          arrayOfOptions={['good', 'neutral', 'bad']}
+          onBtnClick={onBtnClick}
+        />
+      </Section>
 
-    return (
-      <Container>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            arrayOfOptions={['good', 'neutral', 'bad']}
-            onBtnClick={this.onBtnClick}
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedback={countPositiveFeedback()}
           />
-        </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Container>
+  );
+};
 
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positiveFeedback={positiveFeedback}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </Container>
-    );
-  }
-}
-
-export default App;
+  
